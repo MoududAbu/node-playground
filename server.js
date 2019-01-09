3f81c514eaf8faea0d067dac618995e7ec327f5f
moudud <moududabu2017@gmail.com>
2019-01-09 12:35:35 +0000

    routing simple

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+const greeting = require('./routes/greeting')
+
+app.post('/greeting', greeting.hello);
 
 app.listen(3000, function () {
     console.log('Listening on port 3000');
